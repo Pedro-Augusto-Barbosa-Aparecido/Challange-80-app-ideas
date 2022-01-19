1623c7049ef299b3a91e48ff1f20e50c62c9a893
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-01-19 12:16:46 -0300

    Configuration of app

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import Document, {
+
+    Html,
+    Head,
+    Main,
+    NextScript
+
+} from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100&display=swap" rel="stylesheet" />
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                          rel="stylesheet"
+                          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                          crossOrigin="anonymous" />
+
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
