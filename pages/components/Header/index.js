88192e4f45bfddd57ab9c80e6f6c0f966cc59237
Pedro Augusto Barbosa Aparecido <pedroaugustobarosa@inatel.br>
2022-01-21 09:06:32 -0300

    Adding login image and login menu

--- pages/components/Header/index.js
+++ pages/components/Header/index.js
@@ -2,7 +2,19 @@ import styles from "./styles.module.css";
 
 import Link from "next/link";
 
+import $ from "jquery";
+
+import { useState } from "react";
+
 export default function Header () {
+    const [display, setDisplay] = useState("none");
+
+    function handleClickLoginContainer (ev) {
+        if ($(ev.target).hasClass("click"))
+            $("#menuLogin").animate({ height: "toggle" }, 500);
+
+    }
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#158894" }}>
@@ -10,6 +22,24 @@ export default function Header () {
                     <Link href={"/"}>
                         <a className="navbar-brand fs-2 font-monospace" href="#">Home</a>
                     </Link>
+                    <div onClick={handleClickLoginContainer} className={`${styles.login} click`}>
+                        <p className={"click"}>Sing-in</p>
+                        <i className={"bi bi-person-circle click"} />
+                        <div className={`${styles.menuLogin}`} id={"menuLogin"} style={{ display }}>
+                            <Link href={"/profile/1"}>
+                                <a>
+                                    <i className={"bi bi-person-workspace"} />
+                                    <p>My profile</p>
+                                </a>
+                            </Link>
+                            <Link href={"/logout"}>
+                                <a>
+                                    <i className={"bi bi-box-arrow-right"} />
+                                    <p>Logout</p>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
